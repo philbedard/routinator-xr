1e199b88ddb27185cee0c5aa63455f29f3bad83b
Phil Bedard <phbedard@cisco.com>
2020-05-17 17:01:49 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,9 +23,13 @@ else
       echo "No RRDP proxy set, set using -e RRDP_PROXY=proxy (URI form) in docker run if required"
    fi
 
-   NS1="${NS1:-1.1.1.1}"
-   echo "Using default "$NS1" as DNS server, override with -e NS1=nameserver to override default of 1.1.1.1"
-   echo "nameserver "$NS1"" >> /etc/resolv.conf
+   NS1="${NS1:-208.67.222.222}"
+   echo "Using default "$NS1" as primary DNS server, override with -e NS1=nameserver to override default of 208.67.222.222"
+   echo "nameserver "$NS1"" > /etc/resolv.conf
+   
+   NS2="${NS2:-208.67.220.220}"
+   echo "Using default "$NS2" as secondary DNS server, override with -e NS2=nameserver to override default of 208.67.220.220"
+   echo "nameserver "$NS2"" >> /etc/resolv.conf
 
    echo "Creating RPKI data directory at /misc/app_host/rpki"
    mkdir -p /data/rpki/tals
