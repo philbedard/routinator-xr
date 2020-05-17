de3a1f8ec41b45dbdad6db5eca030bfb6d3bb838
Phil Bedard <phbedard@cisco.com>
2020-05-17 17:45:54 -0400

    Fixed typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,13 +6,13 @@ then
   exec "$@"
 else
    VRF="${VRF:-global-vrf}"
-   echo "Using $VRF as namespace, override with -e VRF=vrf if using a different VRF for TPA"
+   echo "Using $VRF as namespace, override default of global-vrf with -e VRF=vrf if using a different VRF for TPA"
    
    RTR_PORT="${RTR_PORT:-3323}"
-   echo "Using $RTR_PORT as RTR server port, override with -e RTR_PORT=port"  
+   echo "Using $RTR_PORT as RTR server port, override default of 3323 with -e RTR_PORT=port"  
    
    HTTP_PORT="${HTTP_PORT:-9556}"
-   echo "Using $HTTP_PORT as Routinator HTTP server port, override with -e HTTP_PORT=port"  
+   echo "Using $HTTP_PORT as Routinator HTTP server port, override default of 9556 with -e HTTP_PORT=port"  
 
    if [[ -v RSYNC_PROXY ]];
    then
@@ -30,11 +30,11 @@ else
    fi
 
    NS1="${NS1:-208.67.222.222}"
-   echo "Using default "$NS1" as primary DNS server, override with -e NS1=nameserver to override default of 208.67.222.222"
+   echo "Using "$NS1" as primary DNS server, override with -e NS1=nameserver to override default of 208.67.222.222"
    echo "nameserver "$NS1"" > /etc/resolv.conf
    
    NS2="${NS2:-208.67.220.220}"
-   echo "Using default "$NS2" as secondary DNS server, override with -e NS2=nameserver to override default of 208.67.220.220"
+   echo "Using "$NS2" as secondary DNS server, override with -e NS2=nameserver to override default of 208.67.220.220"
    echo "nameserver "$NS2"" >> /etc/resolv.conf
 
    echo "Creating RPKI data directory at /misc/app_host/rpki"
@@ -54,5 +54,5 @@ else
                                         --base-dir /data/rpki/ \
                                         --verbose \
                                         $RRDP_ARG \
-                                        server --rtr 0.0.0.0:$RTR_PORT--http 0.0.0.0:$HTTP_PORT
+                                        server --rtr 0.0.0.0:$RTR_PORT --http 0.0.0.0:$HTTP_PORT
 fi
